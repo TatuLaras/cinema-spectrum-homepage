7de82968dea192d0a2cc108d119632696e69957f
Tatu Laras <tatu.laras@gmail.com>
2024-04-12 19:22:35 +0300

    Improved download panel, added new screenshots

--- src/features/info/components/Info.tsx
+++ src/features/info/components/Info.tsx
@@ -1,4 +1,10 @@
-import { motion, useMotionValue, useScroll, useSpring, useTransform } from 'framer-motion';
+import {
+    motion,
+    useMotionValue,
+    useScroll,
+    useSpring,
+    useTransform,
+} from 'framer-motion';
 import getVariants from '../variants';
 import SplitFeature from './SplitFeature';
 import UnknownFeature from './UnknownFeature';
@@ -6,8 +12,10 @@ import UnknownFeature from './UnknownFeature';
 import '../styles/info.scss';
 import screenshot1 from '../../../assets/screenshots/screenshot1.png';
 import screenshot2 from '../../../assets/screenshots/screenshot2.png';
-import { memo } from 'react';
+import screenshotTvShowDesktop from '../../../assets/screenshots/screenshot-tvshow-desktop.png';
+import screenshotTvShowTv from '../../../assets/screenshots/screenshot-tvshow-tv.png';
 
+import { memo } from 'react';
 
 function Info() {
     const { scrollY } = useScroll();
@@ -22,24 +30,30 @@ function Info() {
     const layer1 = useMotionValue(-0.9);
     const layer2 = useMotionValue(-0.08);
     const layer3 = useMotionValue(-0.01);
-    const h2Position = useTransform(() => h2Offset.get() + (progress.get() - base.get()) * layer1.get());
-    const blobPosition = useTransform(() => (progress.get() - base.get()) * layer2.get());
-    const bgPosition = useTransform(() =>  (progress.get() - base.get()) * layer3.get());
+    const h2Position = useTransform(
+        () => h2Offset.get() + (progress.get() - base.get()) * layer1.get(),
+    );
+    const blobPosition = useTransform(
+        () => (progress.get() - base.get()) * layer2.get(),
+    );
+    const bgPosition = useTransform(
+        () => (progress.get() - base.get()) * layer3.get(),
+    );
 
     return (
         <motion.div
-            className='info1'
+            className="info1"
             variants={getVariants('info1')}
             initial={{ opacity: 0 }}
         >
-            <div className='wrapper'>
-                <div className='blob-container'>
+            <div className="wrapper">
+                <div className="blob-container">
                     <motion.div
-                        className='bottom-layer'
+                        className="bottom-layer"
                         style={{ y: bgPosition }}
                     ></motion.div>
                     <motion.div
-                        className='blob blob-4'
+                        className="blob blob-4"
                         initial={{ rotate: -45 }}
                         style={{ y: blobPosition }}
                         variants={getVariants('blob4')}
@@ -47,20 +61,20 @@ function Info() {
                 </div>
             </div>
 
-            <motion.div className='content' style={{ y: h2Position }}>
-                <motion.h2 className='panel' variants={getVariants('h2')}>
-                    The <span className='hl'>Media Center</span> You've Been
+            <motion.div className="content" style={{ y: h2Position }}>
+                <motion.h2 className="panel" variants={getVariants('h2')}>
+                    The <span className="hl">Media Center</span> You've Been
                     Looking For
                 </motion.h2>
                 <SplitFeature
-                    title='Your local streaming service'
+                    title="Your local streaming service"
                     img1={screenshot2}
                     img2={screenshot1}
                 />
                 <SplitFeature
-                    title='Works well on both the TV and as a desktop app'
-                    img1={screenshot1}
-                    img2={screenshot2}
+                    title="Works well on both the TV and as a desktop app"
+                    img1={screenshotTvShowDesktop}
+                    img2={screenshotTvShowTv}
                 />
                 <UnknownFeature />
             </motion.div>
